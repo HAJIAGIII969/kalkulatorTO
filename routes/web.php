c7adde70bfe04d66c7b147797bc5786ebbb1cc88
haggie auberta <pilotgaming231@gmail.com>
2024-04-18 14:36:06 +0700

    m

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('kalkulator.index');
+    
+});
+
+Route::get('index2', function () {
     return view('kalkulator.index2');
+    
 });
 Route::resource('proses',ProsesController::class);
